7fd2bd8d200ca5157122fb1a45539decde100b82
Pashutkaфф <shchirovpavel@gmail.com>
2023-02-19 19:46:43 +0300

    Выполнен второй пункт 1 лабы

--- Lab_1/Lab_1.py
+++ Lab_1/Lab_1.py
@@ -1,1 +1,42 @@
 print("Hello World")
+
+signs = ["+", "-", "/", "*"]
+
+def calc(a, b, operation):
+    if operation == "-":
+        return a - b
+    elif operation == "+":
+        return a + b
+    elif operation == "/":
+        return a / b
+    else:
+        return a * b
+    
+while True:
+    try:
+        a = float(str(input("Enter the first argument: ")))
+
+        try:                 
+            b = float(str(input("Enter second argument: ")))
+            operation = input("Enter operation(+, -, /, *): ")
+
+            if operation in signs:
+                print(calc(float(a), int(b), operation))
+            else:
+                print("Wrong input!")
+                continue
+
+            answer = input("Wanna calculate more(Y/n)?")
+            
+            if answer == "Y":
+                continue
+            else:
+                break
+        except ValueError:
+            print("Wrong input!")
+            continue 
+    except ValueError:
+        print("Wrong input!")
+        continue
+
+print("Program ended.")
