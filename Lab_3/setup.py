de7456264383b2ca926ab27ca9cc8208a0f58ea3
Pashutkaфф <shchirovpavel@gmail.com>
2023-05-30 23:09:24 +0300

    Hot Fix

--- Lab_3/setup.py
+++ Lab_3/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    name="Shchirov's_serializer",
+    name="Shchirov_serializer",
     version="1.1.0",
     description="python library for serialization",
     url="https://github.com/PaShampusik/PythonLabs/Lab_3",
@@ -11,7 +11,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent"
-    ]
+    ],
     packages=["serializers/json_serializer", "serializers/base",
               "serializers/xml_serializer", "serializers"],
     include_package_data=True
